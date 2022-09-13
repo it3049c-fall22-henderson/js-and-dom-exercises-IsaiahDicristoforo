a87d4596dca368e08fc4506b6176c4d53bc364b4
Isaiah <48969369+IsaiahDicristoforo@users.noreply.github.com>
2022-09-13 12:09:11 -0400

    Password exercise

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,9 +1,23 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
+const passwordInput = document.getElementById("password")
+const revealButton = document.getElementById("togglePassword")
 
 
 // 🤖: Create an Event Listener on the password reveal button
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
+revealButton.addEventListener('click', function(e){
+	const inputType = passwordInput.getAttribute("type")
+
+	if(inputType != null){
+		if (inputType == "password"){
+			passwordInput.setAttribute("type", "text")
+
+		}else{
+			passwordInput.setAttribute("type", "password")
+		}
+	}
+})
 
 // Provided for you - prevent form submit
 const form = document.querySelector("form");
